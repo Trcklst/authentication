3e4ec79e42c88ea4028a6b683419a3ad9b1d91f6
sybernatuz <decultot.louis@gmail.com>
2020-07-13 14:23:31 +0200

    subscription

--- src/main/java/com/trcklst/authentication/core/feign/GetSubscriptionFeignService.java
+++ src/main/java/com/trcklst/authentication/core/feign/GetSubscriptionFeignService.java
@@ -3,11 +3,11 @@ package com.trcklst.authentication.core.feign;
 import com.trcklst.getsubscription.api.GetSubscriptionDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient("subscription")
 public interface GetSubscriptionFeignService {
 
-    @GetMapping("/api/subscription/{userId}")
-    GetSubscriptionDto getSubscription(@PathVariable Integer userId);
+    @GetMapping("/api/subscription/")
+    GetSubscriptionDto getSubscription(@RequestHeader("userId") Integer userId);
 }
